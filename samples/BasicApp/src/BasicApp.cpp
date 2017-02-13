44245f6bdf3195657b36be8e04ebe8b7d66e68f9
Hai Nguyen <chaoticbob@me.com>
2017-02-12 18:35:22 -0800

    Filled out tinyci.hpp to be minimally operational

--- samples/BasicApp/src/BasicApp.cpp
+++ samples/BasicApp/src/BasicApp.cpp
@@ -7,6 +7,7 @@ using namespace ci::app;
 
 class BasicApp : public App {
 public:
+  void setup() override;
 	void mouseDrag( MouseEvent event ) override;
 	void keyDown( KeyEvent event ) override;
 
@@ -17,6 +18,36 @@ private:
 
 void prepareSettings( BasicApp::Settings* settings )
 {
+  int stopMe = 1;
+}
+
+void BasicApp::setup()
+{
+  //int stopMe = 1;
+  fs::path p = "hello";
+  std::string s = p.str();
+  p = "c:\\somefile.txt";
+  s = p.str();
+  p = "c:\\Windows\\somefile.txt";
+  s = p.str();
+  p = "c:\\Windows\\com\\\\\\other\\\\somefile.txt";
+  s = p.str();
+  p = "//usr/share/lib/gcc///lib.a";
+  s = p.str();
+  p = p / "milk";
+  s = p.str();
+  p /= "cat";
+  s = p.str();
+  p = "/orig";
+  //p /= "/rootpath";
+  s = p;
+
+  p = "C:\\";
+  s = p;
+  bool b = p.is_root();
+
+  s = getAssetPath("file.txt");
+
   int stopMe = 1;
 }
 
